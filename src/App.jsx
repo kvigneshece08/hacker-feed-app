f58ad8a768093fc3115744ccedce0d95d49d1b30
kvigneshece08 <kvigneshece08@gmail.com>
2020-04-13 12:31:58 -0400

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Home from './home/home';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Home />
+      </header>
+    </div>
+  );
+}
+
+export default App;

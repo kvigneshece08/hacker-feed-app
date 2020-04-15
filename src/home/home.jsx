ab3e070cc0808bd34af87a6da8c8da9ac276cdea
kvigneshece08 <kvigneshece08@gmail.com>
2020-04-15 05:44:27 -0400

    Algolia Api intergrated and upvote added onclick.

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { MDBDataTable } from 'mdbreact';
+import _ from 'lodash';
+import moment from 'moment';
 
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = { isLoading: true};
+    this.state = { isLoading: true };
+    this.upvoke = this.upvoke.bind(this);
   }
 
   componentDidMount() {
-    console.log('test');
     fetch(`http://hn.algolia.com/api/v1/items/1`)
       .then(response => response.json())
       .then(data =>
@@ -20,85 +22,96 @@ class Home extends Component {
       .catch(error => this.setState({ error, isLoading: false }));
   }
 
+  upvoke() {
+    this.setState(prevState => ({
+      feeds: {                  
+        ...prevState.feeds,    
+        points: prevState.feeds.points + 1
+      }
+    }))
+  }
+
   render() {
 
     const { isLoading, feeds, error } = this.state;
-    console.log(feeds); // api data to be integrated
-    var feedData = {
-      columns: [
-        {
-          label: 'No of comments',
-          field: 'comments',
-          sort: 'asc',
-          width: 150
-        },
-        {
-          label: 'Upvotes',
-          field: 'upvotes',
-          sort: 'asc',
-          width: 270
-        },
-        {
-          label: 'Action',
-          field: 'action',
-          width: 50
-        },
-        {
-          label: 'Title',
-          field: 'title',
-          sort: 'asc',
-          width: 50
-        },
-        {
-          label: 'Domain',
-          field: 'domain',
-          width: 50
-        },
-        {
-          label: 'Username',
-          field: 'username',
-          width: 50
-        },
-        {
-          label: 'Time posted',
-          field: 'timePosted',
-          width: 50
-        }
-      ],
-      rows: [{
-        comments: 34,
-        upvotes: 82,
-        action: <i class="fas fa-caret-up fa-2x"></i>,
-        title: "companies are using smartphones locations to help the advertisers to reach them",
-        domain: <a href="www.mytimes.com">mytimes.com</a>,
-        username: "pcl",
-        timePosted: "3 hours ago",
-       },
-       {
-        comments: 11,
-        upvotes: 12,
-        action: <i class="fas fa-caret-up fa-2x"></i>,
-        title: "Madoff's victims are getting closed $19B back",
-        domain: <a href="www.bloomberg.com">bloomberg.com</a>,
-        username: "motiw",
-        timePosted: "1 hours ago",
-       }]
+
+    var columnData = [
+      {
+        label: 'No of comments',
+        field: 'comments',
+        sort: 'asc',
+        width: 150
+      },
+      {
+        label: 'Upvotes',
+        field: 'upvotes',
+        sort: 'asc',
+        width: 270
+      },
+      {
+        label: 'Action',
+        field: 'action',
+        width: 50
+      },
+      {
+        label: 'Title',
+        field: 'title',
+        sort: 'asc',
+        width: 50
+      },
+      {
+        label: 'Domain',
+        field: 'domain',
+        width: 50
+      },
+      {
+        label: 'Username',
+        field: 'username',
+        width: 50
+      },
+      {
+        label: 'Time posted',
+        field: 'timePosted',
+        width: 50
+      }
+    ];
+    //column data for the datatable
+
+    var rowData = [];
+    if (feeds) {
+      rowData.push({
+        comments: feeds.children ? _.size(feeds.children) : 0,
+        upvotes: feeds.points,
+        action: <i className="fas fa-caret-up fa-2x" onClick={this.upvoke}></i>,
+        title: feeds.title,
+        domain: <a href={feeds.url}>{feeds.url}</a>,
+        username: feeds.author,
+        timePosted: moment(feeds.created_at, "YYYYMMDD").fromNow()
+      })
     }
+    //row data for the datatable
+    var feedData = {
+      columns: columnData,
+      rows: rowData
+    };
+    //store hidedata value in localstorage
+    var hideData = true;
+    localStorage.setItem('hideData', hideData);
     return (
       <div>
-          <React.Fragment>
-            <h1>Hackers feed</h1>
-            {error ? <p>{error.message}</p> : null}
-            {!isLoading ? (
-              <MDBDataTable responsiveLg responsiveMd responsiveSm
+        <React.Fragment>
+          <h1>Hackers feed</h1>
+          {error ? <p>{error.message}</p> : null}
+          {!isLoading ? (
+            <MDBDataTable responsiveLg responsiveMd responsiveSm
               striped
               hover
               data={feedData}
             />
-            ) : (
-                <h3>Loading...</h3>
-              )}
-          </React.Fragment>
+          ) : (
+              <h3>Loading...</h3>
+            )}
+        </React.Fragment>
       </div>
     );
   }
